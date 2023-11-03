0002f8f0a7a641f3ab5001f4fbd2b8cfd3359188
Thomas Allen <thomasedwardallen@gmail.com>
2023-11-03 16:56:35 +0100

    FEATURE - layout: v2 locale switcher with flag icons

--- components/LocaleSwitch.tsx
+++ components/LocaleSwitch.tsx
@@ -1,24 +1,45 @@
 'use client';
 
-import { Link, usePathname } from '../navigation';
+import { Link, usePathname, useRouter } from '../navigation';
+import { useLocale } from 'next-intl';
+import Image from 'next/image';
+import enPng from '../icons/pngs/en.png';
+import frPng from '../icons/pngs/fr.png';
 
 export default function LocaleSwitch() {
   const pathname = usePathname();
+  const locale = useLocale();
   return (
-    <div>
+    <div className="flex flex-row items-center bg-[#64748b88] rounded-full px-1 scale-[0.75] lg:scale-100 lg:mt-2 lg:mr-2">
       <Link
-        className="mx-2"
+        className={
+          (locale === 'en' ? 'bg-sky-900' : 'bg-gray-600 opacity-50') +
+          ' rounded-full p-1 my-1 mr-1 text-gray-200'
+        }
         locale="en"
         href={pathname}
       >
-        en
+        <Image
+          src={enPng}
+          alt="English"
+          width={24}
+          height={24}
+        />
       </Link>
       <Link
-        className="mx-2"
+        className={
+          (locale === 'fr' ? 'bg-sky-900' : 'bg-gray-600 opacity-50') +
+          ' rounded-full p-1 my-1 text-gray-200'
+        }
         locale="fr"
         href={pathname}
       >
-        fr
+        <Image
+          src={frPng}
+          alt="Français"
+          width={24}
+          height={24}
+        />
       </Link>
     </div>
   );
