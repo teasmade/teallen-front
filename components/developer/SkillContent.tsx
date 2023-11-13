66968f0489a1b58091cf80146fa1d8b70234442c
Thomas Allen <thomasedwardallen@gmail.com>
2023-11-13 11:02:12 +0100

    FEATURE - anim: add framer, setup basic anim for dev skills

--- components/developer/SkillContent.tsx
+++ components/developer/SkillContent.tsx
@@ -1,19 +1,35 @@
+import { motion, AnimatePresence } from 'framer-motion';
+
 export default function SkillContent({
   selectedSkill,
 }: {
   selectedSkill: string;
 }) {
+  const containerStyle =
+    'ml-12 relative flex items-center justify-center w-full h-full';
+  const childStyle = 'absolute w-full h-full flex items-center justify-start';
+
+  const transition = {
+    x: { type: 'spring', stiffness: 300, damping: 30 },
+    opacity: { duration: 0.2 },
+  };
+
+  const initialAndExit = { x: 300, opacity: 0 };
+  const animate = { x: 0, opacity: 1 };
+
   return (
-    <div className="grid grid-cols-5">
-      <pre
-        className={
-          selectedSkill === 'initial'
-            ? 'font-mono text-[16px] scale-x-[1.3] opacity-40 transition-all ease-in duration-[150ms]'
-            : 'font-mono text-[16px] opacity-0 scale-0 transition-all ease-in duration-[150ms]'
-        }
-      >
-        {/* <pre className="font-mono text-[16px] scale-x-[1.3] opacity-40 order-last"> */}
-        {`
+    <div className={containerStyle}>
+      <AnimatePresence>
+        {selectedSkill === 'initial' && (
+          <motion.pre
+            key="initial"
+            initial={initialAndExit}
+            animate={animate}
+            exit={initialAndExit}
+            transition={transition}
+            className={childStyle + ' text-xl text-[#ffffff55] w-3/4'}
+          >
+            {`
                      ,------------------,              ,---------,
                 ,-------------------------,          ,"        ,"|
               ,"                        ,"|        ,"        ,"  |
@@ -33,51 +49,61 @@ export default function SkillContent({
         /_==__==========__==_ooo__ooo=_/'   /___{_)______,"
         '-----------------------------'
       `}
-      </pre>
-      <div
-        className={
-          selectedSkill === 'front'
-            ? 'order-first scale-100 opacity-100 transition-all ease-in duration-[150ms]'
-            : 'scale-0 opacity-0 transition-all ease-in duration-[150ms]'
-        }
-      >
-        FRONT Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
-        quod molestiae dolor eum officiis. Voluptatum quo doloremque ducimus
-        deserunt libero.
-      </div>
-      <div
-        className={
-          selectedSkill === 'back'
-            ? 'order-first scale-100 opacity-100 transition-all ease-in duration-[150ms]'
-            : 'scale-0 opacity-0 transition-all ease-in duration-[150ms]'
-        }
-      >
-        BACK Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
-        quod molestiae dolor eum officiis. Voluptatum quo doloremque ducimus
-        deserunt libero.
-      </div>
-      <div
-        className={
-          selectedSkill === 'full'
-            ? 'order-first scale-100 opacity-100 transition-all ease-in duration-[150ms]'
-            : 'scale-0 opacity-0 transition-all ease-in duration-[150ms]'
-        }
-      >
-        FULL Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
-        quod molestiae dolor eum officiis. Voluptatum quo doloremque ducimus
-        deserunt libero.
-      </div>
-      <div
-        className={
-          selectedSkill === 'soft'
-            ? 'order-first scale-100 opacity-100 transition-all ease-in duration-[150ms]'
-            : 'scale-0 opacity-0 transition-all ease-in duration-[150ms]'
-        }
-      >
-        SOFT Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
-        quod molestiae dolor eum officiis. Voluptatum quo doloremque ducimus
-        deserunt libero.
-      </div>
+          </motion.pre>
+        )}
+        {selectedSkill === 'front' && (
+          <motion.div
+            key="front"
+            initial={initialAndExit}
+            animate={animate}
+            exit={initialAndExit}
+            transition={transition}
+            className={childStyle}
+          >
+            FRONT Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Saepe quod molestiae dolor eum officiis.
+          </motion.div>
+        )}
+        {selectedSkill === 'back' && (
+          <motion.div
+            key="back"
+            initial={initialAndExit}
+            animate={animate}
+            exit={initialAndExit}
+            transition={transition}
+            className={childStyle}
+          >
+            BACK Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+            quod molestiae dolor eum officiis.
+          </motion.div>
+        )}
+        {selectedSkill === 'full' && (
+          <motion.div
+            key="full"
+            initial={initialAndExit}
+            animate={animate}
+            exit={initialAndExit}
+            transition={transition}
+            className={childStyle}
+          >
+            FULL Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+            quod molestiae dolor eum officiis.
+          </motion.div>
+        )}
+        {selectedSkill === 'soft' && (
+          <motion.div
+            key="soft"
+            initial={initialAndExit}
+            animate={animate}
+            exit={initialAndExit}
+            transition={transition}
+            className={childStyle}
+          >
+            SOFT Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+            quod molestiae dolor eum officiis.
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
--- components/developer/SkillDetails.tsx
+++ components/developer/SkillDetails.tsx
@@ -27,15 +27,15 @@ export default function SkillDetails() {
           {/* Btn-Front */}
           <div
             className={
-              'w-fit bg-sky-700 border-t border-r border-l hover:border-b border-white rounded-tl-3xl hover:scale-110 origin-top-left transition-scale ease-in duration-200' +
-              (selectedSection === 'front' ? ' bg-sky-500' : '')
+              'w-fit border-t border-r border-l hover:border-b border-white rounded-tl-3xl hover:scale-110 origin-top-left transition-scale ease-in duration-200' +
+              (selectedSection === 'front' ? ' bg-sky-500' : ' bg-sky-700')
             }
           >
             <button
               onClick={() => onSelectSection('front')}
               className={
-                'border-t border-l border-r border-b border-transparent hover:border-white hover:rounded-br-3xl rounded-tl-3xl p-4 bg-sky-700 hover:bg-sky-800 hover:scale-110 hover:translate-x-2 hover:translate-y-2 origin-top-left transition-scale ease-in duration-200' +
-                (selectedSection === 'front' ? ' bg-sky-500' : '')
+                `border-t border-l border-r border-b border-transparent hover:border-white hover:rounded-br-3xl rounded-tl-3xl p-4 hover:bg-sky-800 hover:scale-110 hover:translate-x-2 hover:translate-y-2 origin-top-left transition-scale ease-in duration-200` +
+                (selectedSection === 'front' ? ' bg-sky-500' : ' bg-sky-700')
               }
             >
               <div className="flex flex-row justify-around w-32">
@@ -51,15 +51,15 @@ export default function SkillDetails() {
           {/* Btn-Back */}
           <div
             className={
-              'w-fit bg-sky-700 border-l border-r border-t hover:border-b border-white hover:scale-110 origin-top-left transition-scale ease-in duration-200' +
-              (selectedSection === 'back' ? ' bg-sky-500' : '')
+              'w-fit border-l border-r border-t hover:border-b border-white hover:scale-110 origin-top-left transition-scale ease-in duration-200' +
+              (selectedSection === 'back' ? ' bg-sky-500' : ' bg-sky-700')
             }
           >
             <button
               onClick={() => onSelectSection('back')}
               className={
-                'border-l border-r border-t hover:border-b border-transparent hover:border-white hover:rounded-br-3xl p-4 bg-sky-700 hover:bg-sky-800 hover:scale-110 hover:translate-x-2 hover:translate-y-2 origin-top-left transition-scale ease-in duration-200' +
-                (selectedSection === 'back' ? ' bg-sky-500' : '')
+                'border-l border-r border-t hover:border-b border-transparent hover:border-white hover:rounded-br-3xl p-4 hover:bg-sky-800 hover:scale-110 hover:translate-x-2 hover:translate-y-2 origin-top-left transition-scale ease-in duration-200' +
+                (selectedSection === 'back' ? ' bg-sky-500' : ' bg-sky-700')
               }
             >
               <div className="flex flex-row justify-around w-32">
@@ -75,15 +75,15 @@ export default function SkillDetails() {
           {/* Btn-Full */}
           <div
             className={
-              'w-fit bg-sky-700 border-l border-r border-t hover:border-b border-white hover:scale-110 origin-top-left transition-scale ease-in duration-200' +
-              (selectedSection === 'full' ? ' bg-sky-500' : '')
+              'w-fit border-l border-r border-t hover:border-b border-white hover:scale-110 origin-top-left transition-scale ease-in duration-200' +
+              (selectedSection === 'full' ? ' bg-sky-500' : ' bg-sky-700')
             }
           >
             <button
               onClick={() => onSelectSection('full')}
               className={
-                'border-l border-r border-t hover:border-b border-transparent hover:border-white hover:rounded-br-3xl p-4 bg-sky-700 hover:bg-sky-800 hover:scale-110 hover:translate-x-2 hover:translate-y-2 origin-top-left transition-scale ease-in duration-200' +
-                (selectedSection === 'full' ? ' bg-sky-500' : '')
+                'border-l border-r border-t hover:border-b border-transparent hover:border-white hover:rounded-br-3xl p-4 hover:bg-sky-800 hover:scale-110 hover:translate-x-2 hover:translate-y-2 origin-top-left transition-scale ease-in duration-200' +
+                (selectedSection === 'full' ? ' bg-sky-500' : ' bg-sky-700')
               }
             >
               <div className="flex flex-row justify-around w-32">
@@ -99,15 +99,15 @@ export default function SkillDetails() {
           {/* Btn-Soft */}
           <div
             className={
-              'w-fit bg-sky-700 border-t border-l border-r border-b border-white border-b hover:scale-110 origin-top-left transition-scale ease-in duration-200' +
-              (selectedSection === 'soft' ? ' bg-sky-500' : '')
+              'w-fit border-t border-l border-r border-b border-white border-b hover:scale-110 origin-top-left transition-scale ease-in duration-200' +
+              (selectedSection === 'soft' ? ' bg-sky-500' : ' bg-sky-700')
             }
           >
             <button
               onClick={() => onSelectSection('soft')}
               className={
-                'border-t border-l border-r hover:border-b border-transparent hover:border-white p-4 bg-sky-700 hover:bg-sky-800 hover:scale-110 hover:translate-x-2 hover:translate-y-2 hover:rounded-br-3xl origin-top-left transition-scale ease-in duration-200' +
-                (selectedSection === 'soft' ? ' bg-sky-500' : '')
+                'border-t border-l border-r hover:border-b border-transparent hover:border-white p-4 hover:bg-sky-800 hover:scale-110 hover:translate-x-2 hover:translate-y-2 hover:rounded-br-3xl origin-top-left transition-scale ease-in duration-200' +
+                (selectedSection === 'soft' ? ' bg-sky-500' : ' bg-sky-700')
               }
             >
               <div className="flex flex-row justify-around w-32">
