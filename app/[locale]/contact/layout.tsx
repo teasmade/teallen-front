2f85076ed114d37b11533d559e66c827780ca34a
Thomas Allen <thomasedwardallen@gmail.com>
2023-11-27 16:05:42 +0100

    FEATURE - contact: contact form, submission, thanks pages

--- app/[locale]/contact/layout.tsx
+++ app/[locale]/contact/layout.tsx
@@ -10,7 +10,7 @@ export default function ContactLayout({ children }: { children: ReactNode }) {
         contentSection="Contact"
         sectionColor="sky_darker"
       />
-      <div className="mx-2 sm:mr-auto lg:ml-auto mt-4 sm:w-4/5 lg:w-3/5 2xl:w-5/12 2xl:mr-auto">
+      <div className="mx-2 sm:mr-auto lg:ml-auto mt-4 sm:w-4/5 lg:w-3/5 2xl:w-5/12 2xl:mr-auto pb-16">
         <>{children}</>
       </div>
     </main>
