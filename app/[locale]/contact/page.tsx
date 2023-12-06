2d4b68d1d1637442e84e6570122f09b5a5e93c34
Thomas Allen <thomasedwardallen@gmail.com>
2023-12-06 09:49:35 +0100

    FEATURE - nodemailer: install, setup mail formsubmit notification

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,6 +1,9 @@
 import { useTranslations, useLocale } from 'next-intl';
 import { PrismaClient } from '@prisma/client';
 import { redirect } from 'next/navigation';
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import Submit from './Submit';
 
 interface FormDataFields {
   email: string;
@@ -10,13 +13,43 @@ interface FormDataFields {
   interest: string;
   message: string;
   _next: string;
+  _subject: string;
 }
 const prisma = new PrismaClient();
+
+// TODO - add recaptcha
 
 export default function ContactPage() {
   async function action(formData: FormData) {
     'use server';
+
+    const email = process.env.NODEMAILER_EMAIL;
+    const password = process.env.NODEMAILER_PASSWORD;
+    if (!email || !password) {
+      console.error('Environment variables for NodeMailer are missing');
+      return { message: 'Server configuration error', status: 500 };
+    }
+
     const data = Object.fromEntries(formData) as unknown as FormDataFields;
+
+    const transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: email,
+        pass: password,
+      },
+    });
+
+    const onError = async (err: unknown) => {
+      const mailOptions: Mail.Options = {
+        from: email,
+        to: email,
+        subject: 'teallen/contact @ error ' + `(${process.env.APP_ENV})`,
+        text: JSON.stringify(err),
+      };
+      await transport.sendMail(mailOptions);
+    };
+
     try {
       const results = await prisma.form.create({
         data: {
@@ -28,28 +61,47 @@ export default function ContactPage() {
           message: data?.message,
         },
       });
-    } catch (error) {
-      console.error(error);
-      // TODO: handle error
+
+      const mailHtml = `
+      <table><tr><th></th><th></th></tr><tr><td>Email:</td><td>${data?.email}</td></tr><tr><td>Phone:</td><td>${data?.phone}</td></tr><tr><td>First Name:</td><td>${data?.first}</td></tr><tr><td>Last Name:</td><td>${data?.last}</td></tr><tr><td>Interest:</td><td>${data?.interest}</td></tr><tr><td>Message:</td><td>${data?.message}</td></tr></table>
+      `;
+
+      const mailOptions: Mail.Options = {
+        from: email,
+        to: email,
+        subject: data?._subject,
+        replyTo: data?.email,
+        text: mailHtml,
+        html: mailHtml,
+      };
+
+      await transport.sendMail(mailOptions);
+      return { message: 'Form submitted', status: 200 };
+    } catch (error: unknown) {
+      onError(error);
+      return { message: 'Form submit failed', status: 500 };
     } finally {
       redirect(data._next);
     }
   }
 
   const t = useTranslations('Contact.form');
+
   const locale = useLocale();
   const returnUri = () => {
     return `/${locale}/contact/thanks`;
   };
+
   const contactSubject = () => {
     const dateTimeString = () => {
       const now = new Date();
       const date = now.toISOString().split('T')[0];
       const time = now.toTimeString().split(' ')[0];
-      return `${time} - ${date}`;
+      return `${time}-${date}`;
     };
     const dateTime = dateTimeString();
-    return `teallen/contact @ ${dateTime}`;
+    const env = process.env.APP_ENV !== 'production' ? `devtest-` : '';
+    return `${env}teallen/contact @${dateTime}`;
   };
 
   return (
@@ -57,7 +109,7 @@ export default function ContactPage() {
       <form
         className="mt-4 flex flex-col items-center gap-4 form-control w-full"
         action={action}
-        method="POST"
+        // method="POST"
       >
         <input
           type="hidden"
@@ -171,6 +223,7 @@ export default function ContactPage() {
           >
             {t('submit')}
           </button>
+          <Submit />
         </div>
       </form>
     </div>
